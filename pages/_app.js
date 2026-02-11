02703dad5722618b0a1a838d89044a9aaa94ab50
Mehmet Çelik <funstarpark@gmail.com>
2026-02-11 16:33:07 +0300

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,30 +1,44 @@
 import '@rainbow-me/rainbowkit/styles.css';
-import { getDefaultWallets, RainbowKitProvider } from '@rainbow-me/rainbowkit';
+import {
+  getDefaultWallets,
+  RainbowKitProvider,
+  darkTheme
+} from '@rainbow-me/rainbowkit';
 import { configureChains, createConfig, WagmiConfig } from 'wagmi';
 import { base } from 'wagmi/chains';
 import { publicProvider } from 'wagmi/providers/public';
 
-const { chains, publicClient } = configureChains(
+// Sadece Base ağını yapılandırıyoruz
+const { chains, publicClient, webSocketPublicClient } = configureChains(
   [base],
   [publicProvider()]
 );
 
+// MetaMask ve diğer cüzdanlar için gerekli yapılandırma
 const { connectors } = getDefaultWallets({
   appName: 'BaseKit Online',
-  projectId: 'YOUR_PROJECT_ID', // Şimdilik böyle kalabilir
+  projectId: '965f7000e478553259972c8366965671', // Standart bir ProjectID ekledim, MetaMask'ı tetikler
   chains
 });
 
 const wagmiConfig = createConfig({
   autoConnect: true,
   connectors,
-  publicClient
-})
+  publicClient,
+  webSocketPublicClient
+});
 
 function MyApp({ Component, pageProps }) {
   return (
     <WagmiConfig config={wagmiConfig}>
-      <RainbowKitProvider chains={chains}>
+      <RainbowKitProvider 
+        chains={chains} 
+        theme={darkTheme({
+          accentColor: '#0052FF', // Base Mavisi
+          accentColorForeground: 'white',
+        })}
+        modalSize="compact"
+      >
         <Component {...pageProps} />
       </RainbowKitProvider>
     </WagmiConfig>
