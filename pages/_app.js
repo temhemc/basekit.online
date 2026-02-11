c4f670acc9c912a8e93e90916acff66fc9d70961
Mehmet Çelik <funstarpark@gmail.com>
2026-02-12 01:59:33 +0300

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,14 +1,5 @@
 import '@rainbow-me/rainbowkit/styles.css';
-import {
-  connectorsForWallets,
-  RainbowKitProvider,
-  darkTheme
-} from '@rainbow-me/rainbowkit';
-import {
-  metaMaskWallet,
-  coinbaseWallet,
-  rainbowWallet
-} from '@rainbow-me/rainbowkit/wallets';
+import { getDefaultWallets, RainbowKitProvider, darkTheme } from '@rainbow-me/rainbowkit';
 import { configureChains, createConfig, WagmiConfig } from 'wagmi';
 import { base } from 'wagmi/chains';
 import { publicProvider } from 'wagmi/providers/public';
@@ -18,16 +9,11 @@ const { chains, publicClient } = configureChains(
   [publicProvider()]
 );
 
-const connectors = connectorsForWallets([
-  {
-    groupName: 'Önerilen Cüzdanlar',
-    wallets: [
-      metaMaskWallet({ projectId: '965f7000e478553259972c8366965671', chains }),
-      coinbaseWallet({ appName: 'BaseKit Online', chains }),
-      rainbowWallet({ projectId: '965f7000e478553259972c8366965671', chains }),
-    ],
-  },
-]);
+const { connectors } = getDefaultWallets({
+  appName: 'BaseKit Online',
+  projectId: '965f7000e478553259972c8366965671', // RainbowKit/MetaMask bağlantısı için şart
+  chains
+});
 
 const wagmiConfig = createConfig({
   autoConnect: true,
@@ -38,13 +24,7 @@ const wagmiConfig = createConfig({
 function MyApp({ Component, pageProps }) {
   return (
     <WagmiConfig config={wagmiConfig}>
-      <RainbowKitProvider 
-        chains={chains} 
-        theme={darkTheme({
-          accentColor: '#0052FF',
-          accentColorForeground: 'white',
-        })}
-      >
+      <RainbowKitProvider chains={chains} theme={darkTheme()}>
         <Component {...pageProps} />
       </RainbowKitProvider>
     </WagmiConfig>
